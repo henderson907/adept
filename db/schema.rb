2b93e906303a17b5a6a33dc5eb1c221119170c05
augustin romaneschi <augaugro@gmail.com>
2022-11-24 18:47:49 +0100

    my profile

--- db/schema.rb
+++ db/schema.rb
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_195916) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.string "first_name"
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
